c0bed926351fb5985c294e6710b80473c13373be
Achraf-Assal <achrafassa.le4@gmail.com>
2024-10-16 23:05:53 +0200

    seting up the extantion

---
+++ background.js
@@ -0,0 +1,77 @@
+
+// function ManegingJsonFil(bookmarks) {
+//   try {
+//     const jsonBookmarks = JSON.stringify(bookmarks);
+//     // const blob = new Blob([jsonBookmarks], { type: 'application/json' });
+//     // const url = URL.createObjectURL(blob);
+//     const dataURI = 'data:application/json;charset=utf-8,' + encodeURIComponent(jsonBookmarks);
+//     chrome.downloads.download({
+//       url: dataURI,
+//       filename: 'bookmarks.json'
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+
+function CreateBookmark(bookmark) {
+  const { children, ...createdData } = bookmark; // Destructure 'children' from the bookmark object
+
+  // Check if the bookmark is a folder or a bookmark
+  if (children && Array.isArray(children)) {
+    // If it's a folder, first create the folder
+    chrome.bookmarks.create(createdData, (createdFolder) => {
+      // Recursively add children to the folder
+      children.forEach((child) => {
+        child.parentId = createdFolder.id; // Set parentId to the id of the created folder
+        CreateBookmark(child);
+      });
+    });
+  } else {
+    // If it's a bookmark, simply create it
+    chrome.bookmarks.create(createdData);
+  }
+}
+
+function GetCheldrenNode(bookmark) {
+  try {
+    const bookmarksJSON = [];
+    bookmark.forEach((node) => {
+      const _bookmark = {
+        // id: node.id || null,
+        title: node.title,
+        url: node.url || null,
+        parentId: node.parentId? node.parentId :null,
+        children: node.children ? GetCheldrenNode(node.children) : null
+      }
+
+      bookmarksJSON.push(_bookmark);
+
+    });
+    return bookmarksJSON;
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'getBookmarks') {
+    chrome.bookmarks.getTree(function (bookmarkTreeNodes) {
+      const bookmarksJSON = GetCheldrenNode(bookmarkTreeNodes);
+      // ManegingJsonFil(bookmarksJSON);
+      console.log(bookmarksJSON);
+    });
+  }
+  if (message.action === 'createBookmarks') {
+
+    const jsonData = message.jsonData;
+    jsonData.forEach((bookmark)=>{
+      CreateBookmark(bookmark)
+    });
+  }
+});
+
---
+++ content.js
@@ -0,0 +1,59 @@
+function debug(text) {
+  const p = document.createElement('p');
+  p.textContent = text;
+  document.body.appendChild(p);
+}
+
+function showLoading() {
+  document.getElementById('loading').style.display = 'block';
+}
+
+// Hide loading indicator
+function hideLoading() {
+  document.getElementById('loading').style.display = 'none';
+}
+
+
+function ImportJsonFile(event) {
+  try {
+    showLoading()
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async function (event) {
+      const jsonContents = await event.target.result;
+      const jsonData = await JSON.parse(jsonContents);
+      await chrome.runtime.sendMessage({ action: 'createBookmarks', jsonData }, (bookmarks) => {});
+    };
+
+    reader.readAsText(file);
+    hideLoading() 
+  } catch (error) {
+    debug(error)
+  }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const exportBtn = document.getElementById('exportBtn');
+  exportBtn.addEventListener('click', () => {
+    chrome.runtime.sendMessage({ action: 'getBookmarks' }, (bookmarks) => { });
+  });
+
+  const fileInput = document.getElementById('fileInput');
+  const importBtn = document.getElementById('importBtn');
+  importBtn.addEventListener('click', () => {
+    //  chrome.runtime.sendMessage({ action: 'createBookmarks' }, (bookmarks) => {});
+    fileInput.click();
+
+  });
+
+  fileInput.addEventListener('change', ImportJsonFile)
+
+});
+
+
+
+
+
+
